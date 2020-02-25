932e64c23b9c43e404c5001558740e5b86a4e15e
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-02-25 10:30:08 -0800

    app.module.ts
    Added Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DisplayShowsInfoService} from './display-shows.service'
 import { AppComponent } from './app.component';
-
+import { DisplayShowsInfoComponent } from './display-shows-info/display-shows-info.component';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayShowsInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DisplayShowsInfoComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

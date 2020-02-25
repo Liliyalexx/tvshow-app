6d29d6f15fa9a5b6821d7b48faecdecf90898675
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-02-25 10:28:17 -0800

    Interfaces
    Added 3 Interfaces

---
+++ src/app/IDisplayShowsData.ts
@@ -0,0 +1,11 @@
+export interface IDisplayShowsData {
+  name: string;
+  genres: [string, string];
+  lanuage: string;
+  status: string;
+  runtime: number;
+  image: {
+    medium: string;
+  };
+  summury: string;
+}
---
+++ src/app/idisplay-shows-data.ts
@@ -0,0 +1,14 @@
+export interface IDisplayShowsData {
+  name: string,
+  genres: [
+    string, string
+  ],
+  lanuage: string,
+  status: string,
+  runtime: number
+  image: {
+    medium: string
+  },
+  summury: string
+
+}

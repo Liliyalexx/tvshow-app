4cc97b5d148c70acb393036bd1414e5d7eef2301
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-02-25 20:48:44 -0800

    idisplay-shows-data

---
+++ src/app/idisplay-shows-data.ts
@@ -0,0 +1,15 @@
+export interface IDisplayShowsData {
+  name: string,
+  genres: [
+    string, string
+  ],
+  lanuage: string,
+  status: string,
+  runtime: number
+  image: {
+    medium: string
+  },
+  summary: string
+
+}
+

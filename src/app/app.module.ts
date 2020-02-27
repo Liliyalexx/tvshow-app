4805fb78e116cb7c7e1def9b92cb06241d8c842d
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-02-26 16:16:22 -0800

    changing errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { AppComponent } from './app.component';
 import { DisplayShowsInfoComponent } from './display-shows-info/display-shows-info.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ShowSearchComponent } from './show-search/show-search.component';
+import 'hammerjs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 @NgModule({
@@ -14,7 +20,11 @@ import { ShowSearchComponent } from './show-search/show-search.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [DisplayShowsInfoComponent],
   bootstrap: [AppComponent]
